82220ac3da89e83cd8b9a0d65bb13eebc2d8dab4
daniela <daniirb19@gmail.com>
2022-04-19 18:17:10 -0500

    final version

---
+++ app.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById("burger");
+const nav = document.getElementById("lat-nav");
+
+
+burger.addEventListener("click", ()=> {
+    nav.classList.toggle("active")
+})
